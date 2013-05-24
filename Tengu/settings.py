aacb70b4309939a6c7974426e101fd9c2733c89b
Andrei Picus <office.nightcrawler@gmail.com>
2013-05-24 20:46:13 +0300

    UI: Using local_settings.
    Removed db login info from settings and moved it to local_settings (untracked).
    The file also contains debug and static files settings.

--- Tengu/settings.py
+++ Tengu/settings.py
@@ -12,11 +12,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tengu',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'tengu',
-        'PASSWORD': 'tengu',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -159,3 +159,9 @@ LOGGING = {
         },
     }
 }
+
+try:
+  from local_settings import *
+except ImportError:
+  pass
+
