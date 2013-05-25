1d2f6d166e4ce09e72b900451e501a23a0d3703c
Andrei Picus <office.nightcrawler@gmail.com>
2013-05-25 15:52:26 +0300

    UI: New containment for the resize handler.
    The resize handler now only goes up till below the search box. Also, made some
    optimizations.

--- ui/static/js/layout.js
+++ ui/static/js/layout.js
@@ -2,29 +2,47 @@ var content, leftSidebar, rightSidebar, header, footer, resizeHandle;
 var contentPaddingX, contentPaddingY;
 var leftSidebarPaddingX, leftSidebarPaddingY;
 var rightSidebarPaddingX, rightSidebarPaddingY;
+var handleHight;
 var headerHeight, footerHeight;
 var oldWindowHeight, oldWindowWidth;
 var windowHeight, windowWidth;
+var containment = [0, 0, 0, 0];
 
 var COOKIE_EXPIRE = 3600 * 24 * 30;
 
 
+function calculateContainment() {
+	/**
+	 * Calculates the bounding box for the resize handler.
+	 *
+	 * Update the elements directly so jQuery UI will use the new box. If we were
+	 * to just assign containment a new array, this would create a new object and
+	 * jQuery UI would still be using the old one.
+	 */
+	var marketTree = $("#market-tree");
+	containment[0] = marketTree.offset().left;
+	containment[1] = marketTree.offset().top;
+	containment[2] = leftSidebar.offset().left + leftSidebar.width();
+	containment[3] = leftSidebar.offset().top + leftSidebar.height() -
+					resizeHandle.outerHeight(true);
+}
+
 function adjustLeftSidebar() {
 	var handlePosition = resizeHandle.position().top;
-	var handleHeight = resizeHandle.height();
-
-	// Adjust the resize handler.
-	var distance = Math.ceil((oldWindowHeight - windowHeight) / 2);
+	var distance = Math.floor((oldWindowHeight - windowHeight) / 2);
 	var top = resizeHandle.position().top;
 	var oldTop = top;
 
-	// Don't move the handler below the sidebar.
-	if (top - distance >= 0)
-		top -= distance;
+	// Adjust the resize handler, only if the window has changed.
+	if (distance) {
+		// Don't move the handler below the containment.
+		if (top - distance >= containment[1])
+			top -= distance;
 
-	// Nor above it.
-	if (top > leftSidebar.height())
-		top = leftSidebar.height() - handleHeight;
+		// Nor above it.
+		if (top > containment[3])
+			top = containment[3];
+	}
 
 	$("#left-sidebar-top").height(handlePosition);
 	$("#left-sidebar-bottom").height(leftSidebar.height() - handlePosition - handleHeight);
@@ -72,6 +90,8 @@ $(document).ready(function() {
 	rightSidebar = $("#right-sidebar");
 	resizeHandle = $("#resize-handle");
 
+	handleHeight = resizeHandle.outerHeight(true);
+
 	headerHeight = $("#header").outerHeight(true);
 	footerHeight = $("#footer").outerHeight(true);
 
@@ -94,12 +114,15 @@ $(document).ready(function() {
 	adjustLayout();
 
 	$(window).resize(function() {
+		calculateContainment();
 		adjustLayout();
 	});
 
+	calculateContainment();
+
 	$("#resize-handle").draggable({
 			axis: "y",
-			containment: $("#left-sidebar"),
+			containment: containment,
 			drag: adjustLeftSidebar,
 			stop: updateResizeHandle
 	});
