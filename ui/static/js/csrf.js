3d7552caac95c5a52343bcbee6d40236e7db1100
Andrei Picus <office.nightcrawler@gmail.com>
2013-05-25 12:55:19 +0300

    UI: Add CSRF headers to POST calls.

---
+++ ui/static/js/csrf.js
@@ -0,0 +1,16 @@
+var csrftoken = $.cookie("csrftoken");
+
+function csrfSafeMethod(method) {
+	// These HTTP methods do not require CSRF protection.
+	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+	crossDomain: false,
+	beforeSend: function(xhr, settings) {
+		if (!csrfSafeMethod(settings.type)) {
+			xhr.setRequestHeader("X-CSRFToken", csrftoken);
+		}
+	}
+});
+
