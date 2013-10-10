e36014cc5784facef4109ad0a6057fbf3ff92fd1
Andrei Picus <office.nightcrawler@gmail.com>
2013-10-10 14:23:30 +0300

    Removed admin URL.

--- Tengu/urls.py
+++ Tengu/urls.py
@@ -13,7 +13,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+
     url(r'^$', 'ui.views.index.home'),
     url(r'^updateWidgets/$', 'ui.views.ajax.updateWidgets'),
     url(r'^updateLeftSidebarResizeHandler/$',
