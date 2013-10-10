9557ca5b9493703f8364cffde026a718d70558bf
Andrei Picus <office.nightcrawler@gmail.com>
2013-10-10 16:23:39 +0300

    Cache market group items and searches.

--- ui/views/ajax.py
+++ ui/views/ajax.py
@@ -1,6 +1,7 @@
 from django.http import Http404, HttpResponse, HttpResponseForbidden
 from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
+from django.core.cache import cache
 
 from dogma.models import TypeAttributes
 from inv.models import MarketGroup, Item
@@ -53,33 +54,65 @@ def updateMarketTree(request):
   return response
 
 def getItems(request, marketGroupID):
-  """Retreives items from a market group."""
+  """Retreives items from a market group.
+
+  First, check whether the list is in cache, returning it directly if it is.
+  Otherwise, retrieve the items from the database and then store them in cache.
+  """
   if not request.is_ajax():
     raise Http404
 
   try:
-    marketGroup = MarketGroup.objects.get(pk=marketGroupID)
-  except MarketGroup.DoesNotExist:
+    marketGroupID = int(marketGroupID)
+  except ValueError:
     raise Http404
 
-  itemQuery = Item.objects.filter(marketGroupID = marketGroup,
-                                  published=True).order_by("typeName")
-  items = getCPUandPG(itemQuery)
+  items = cache.get("items_%d" % marketGroupID)
+
+  if items is None:
+    # List wasn't in cache, let's get it from the database.
+    try:
+      marketGroup = MarketGroup.objects.get(pk=marketGroupID)
+    except MarketGroup.DoesNotExist:
+      raise Http404
+
+    itemQuery = Item.objects.filter(marketGroupID = marketGroup,
+                                    published=True).order_by("typeName")
+    # Augment with CPU and PG info.
+    items = getCPUandPG(itemQuery)
+
+    # Store the list in cache.
+    cache.set("items_%d" % marketGroupID, items)
 
   return render_to_response("items.html", locals())
 
 def searchItems(request, typeName):
   """Searches for items by name.
 
+  First, check whether the list is in cache, returning it directly if it is.
+  Otherwise, retrieve the items from the database and then store them in cache.
+
   You have to check if the items belong to a valid market group i.e. the ones
-  being displayed in the market tree. To do this, you have to resolve the
-  category ID which is linked to by the group ID.
+  being displayed in the market tree. To do this, you have to check the
+  categoryID, which is a foreign key. Thus, we use categoryID_id to save a
+  query. This column, while normally not in the invTypes table, is set by Eos
+  during cache generation.
   """
   if not request.is_ajax():
     raise Http404
 
-  itemQuery = Item.objects.filter(typeName__icontains=typeName, published=True,
-      groupID__categoryID__in=CATEGORIES_ITEMS)
-  items = getCPUandPG(itemQuery)
+  items = cache.get("search_%s" % typeName)
+
+  if items is None:
+    # List wasn't in cache, let's fetch it from the database.
+    itemQuery = Item.objects.filter(typeName__icontains=typeName, published=True,
+        categoryID_id__in=CATEGORIES_ITEMS)
+
+    # Augment with CPU and PG info.
+    items = getCPUandPG(itemQuery)
+
+    # Now store it in cache.
+    cache.set("search_%s" % typeName, items)
+
   return render_to_response("items.html", locals())
 
