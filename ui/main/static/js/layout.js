6160e5ab5bc02952d5f284709310daef3361f3e4
Andrei Picus <office.nightcrawler@gmail.com>
2013-10-15 19:24:09 +0300

    Refactor layout.js.

--- ui/main/static/js/layout.js
+++ ui/main/static/js/layout.js
@@ -1,5 +1,6 @@
 var content, leftSidebar, rightSidebar, header, footer, resizeHandle;
 var contentPaddingX, contentPaddingY;
+var leftSidebarWidth, rightSidebarWidth;
 var leftSidebarPaddingX, leftSidebarPaddingY;
 var rightSidebarPaddingX, rightSidebarPaddingY;
 var handleHight;
@@ -7,6 +8,7 @@ var headerHeight, footerHeight;
 var oldWindowHeight, oldWindowWidth;
 var windowHeight, windowWidth;
 var containment = [0, 0, 0, 0];
+var resizeHandleTopLimit;
 
 var COOKIE_EXPIRE = 3600 * 24 * 30;
 
@@ -27,32 +29,43 @@ function calculateContainment() {
 					resizeHandle.outerHeight(true);
 }
 
-function adjustLeftSidebar() {
-	var handlePosition = resizeHandle.position().top;
-	var distance = Math.floor((oldWindowHeight - windowHeight) / 2);
+function adjustResizeHandle() {
+	/**
+	* Adjust the resize handle's position when the window's height changes.
+	*/
 	var top = resizeHandle.position().top;
 	var oldTop = top;
+
+	var direction = oldWindowHeight < windowHeight ? -1 : 1;
+	var distance = Math.floor(
+			Math.abs(oldWindowHeight - windowHeight) / 2) * direction;
 
 	// Adjust the resize handler, only if the window has changed.
 	if (distance) {
 		// Don't move the handler below the containment.
-		if (top - distance >= containment[1])
+		if (top - distance >= resizeHandleTopLimit)
 			top -= distance;
+		else
+			top = resizeHandleTopLimit;
 
-		// Nor above it.
-		if (top > containment[3])
-			top = containment[3];
+		/**
+		* There shouldn't be any case in which the handler would go over the bottom
+		* limit because it only moves 1/2 of the window difference, so the window
+		* will advance faster than it. Thus, no need to check for the bottom limit.
+		*/
 	}
 
-	$("#left-sidebar-top").height(handlePosition);
-	$("#left-sidebar-bottom").height(leftSidebar.height() - handlePosition - handleHeight);
-
-	resizeHandle.css("top", top);
-	if (oldTop != top)
-		updateResizeHandle();
+	// Update the position and adjust the left sidebar.
+	if (oldTop != top) {
+		resizeHandle.css("top", top);
+		adjustLeftSidebar();
+	}
 }
 
 function updateResizeHandle() {
+	/**
+	* Store the resize handle position in a cookie.
+	*/
 	var top = resizeHandle.position().top;
 
 	$.cookie("leftSidebarResizeHandler", top, {
@@ -61,10 +74,22 @@ function updateResizeHandle() {
 	});
 }
 
-function adjustColumns() {
-	var leftSidebarWidth = leftSidebar.outerWidth(true);
-	var rightSidebarWidth = rightSidebar.outerWidth(true);
+function adjustLeftSidebar() {
+	/**
+	* Resize the top and bottom portions of the left sidebar so they fill the
+	* column's height.
+	*/
+	var handlePosition = resizeHandle.position().top;
 
+	$("#left-sidebar-top").height(handlePosition);
+	$("#left-sidebar-bottom").height(leftSidebar.height() - handlePosition - handleHeight);
+
+}
+
+function adjustColumns() {
+	/**
+	* Resizes the 3 columns so they fill the entire window.
+	*/
 	leftSidebar.height(windowHeight - headerHeight - footerHeight -
 			leftSidebarPaddingY);
 	rightSidebar.height(windowHeight - headerHeight - footerHeight -
@@ -77,17 +102,23 @@ function adjustColumns() {
 }
 
 function adjustLayout() {
+	// Get the new window's dimmensions.
 	windowWidth = $(window).width();
 	windowHeight = $(window).height();
 
+	// Adjust layout.
 	adjustColumns();
-	adjustLeftSidebar();
+	calculateContainment();
+	adjustResizeHandle();
 
+	// Store the window's dimmensions.
 	oldWindowWidth = windowWidth;
 	oldWindowHeight = windowHeight;
 }
 
+
 $(document).ready(function() {
+	// Get some objects and and properties that will never change.
 	content = $("#content");
 	leftSidebar = $("#left-sidebar");
 	rightSidebar = $("#right-sidebar");
@@ -101,27 +132,37 @@ $(document).ready(function() {
 	contentPaddingX = content.outerWidth(true) - content.width();
 	contentPaddingY = content.outerHeight(true) - content.height();
 
+	leftSidebarWidth = leftSidebar.outerWidth(true);
+	rightSidebarWidth = rightSidebar.outerWidth(true);
 	leftSidebarPaddingX = leftSidebar.outerWidth(true) - leftSidebar.width();
 	leftSidebarPaddingY = leftSidebar.outerHeight(true) - leftSidebar.height();
 	rightSidebarPaddingX = rightSidebar.outerWidth(true) - rightSidebar.width();
 	rightSidebarPaddingY = rightSidebar.outerHeight(true) - rightSidebar.height();
 
-	oldWindowHeight = $(window).height();
-	oldWindowWidth = $(window).width();
+	var search = $("#search");
+	resizeHandleTopLimit = search.position().top + search.outerHeight(true);
+
+	oldWindowHeight = windowHeight = $(window).height();
+	oldWindowWidth = windowWidth = $(window).width();
+
+	adjustColumns();
+	calculateContainment();
 
 	// Restore resize handler's position.
 	var top = $.cookie("leftSidebarResizeHandler");
-	if (top)
+	if (top) {
 		resizeHandle.css("top", parseInt(top));
+	} else {
+		// Adjust the resize handle to be at 50% height.
+		resizeHandle.css("top", Math.floor(leftSidebar.height() / 2));
+	}
 
-	adjustLayout();
+	adjustLeftSidebar();
 
 	$(window).resize(function() {
-		calculateContainment();
 		adjustLayout();
+		updateResizeHandle();
 	});
-
-	calculateContainment();
 
 	$("#resize-handle").draggable({
 			axis: "y",
